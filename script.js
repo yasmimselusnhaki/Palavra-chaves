e7eaac239eb42e9aa28c56d49b0d6fca32414185
yasmimselusnhaki <yasmim.selusnhaki@escola.pr.gov.br>
2025-10-22 08:08:22 -0300

    salvar

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const botaoMostraPalavras = document.querySelector("#botao-palavrachave"); 
 
-botaoMostraPalavras.addEventListener("click", mostraPalavrasChave); 
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChave); 
 
 function mostraPalavrasChave() { 
   const texto = document.querySelector('#entrada-de-texto').value;
