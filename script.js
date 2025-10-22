1f201de2c06ec511a1ae2388744cb2d2a8a9529d
yasmimselusnhaki <yasmim.selusnhaki@escola.pr.gov.br>
2025-10-22 07:29:48 -0300

    tirando caracteres

--- script.js
+++ script.js
@@ -5,8 +5,13 @@ botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 function mostraPalavrasChave() { 
   const texto = document.querySelector('#entrada-de-texto').value;
   const campoResultado = document.querySelector('#resultado-palavrachave');
-   const palavras = texto.split(" ");
+   const palavrasChaves = processaTexto("texto");
 
 
-  campoResultado.textContent = palavras.join(', ');
+  campoResultado.textContent = palavrasChave.join(', ');
+
+function processaTexto(texto){
+    let palavras = texto.split(/\P{L}+/u);
+    return palavras;
+}
 } 
