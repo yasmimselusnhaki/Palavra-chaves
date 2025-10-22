0841d96fe4a0b4016a8ada64538e7eb7cc5a28a6
yasmimselusnhaki <yasmim.selusnhaki@escola.pr.gov.br>
2025-10-22 07:56:58 -0300

    ok

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave); 
 
-function mostraPalavrasChave() { 
+function mostraPalavrasChaves() { 
   const texto = document.querySelector('#entrada-de-texto').value;
   const campoResultado = document.querySelector('#resultado-palavrachave');
    const palavrasChaves = processaTexto("texto");
